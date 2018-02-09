ab22e0d68e94dd3c2693d3e64a6515fcff054e56
Cobber <codecobber@gmail.com>
2018-02-09 11:42:20 +0000

    Add files via upload

---
+++ gallery/index.php
@@ -0,0 +1,201 @@
+<?php include('includes/header.inc.php'); ?><div id='start'>
+
+
+       <div id="bck_1" class="bck"><div id="block_1" class="parallaxBase rowNo38">
+
+    <p id="main" style="font-size:1.4em;cursor:pointer" onclick="openNav()">☰ Menu</p>
+    <div class="row rowNo08">
+        <h1>
+          Success is achievable
+        </h1>
+        <p class="tagLine">Business strategists for your business.</p>
+    </div>
+</div></div><div id="bck_2" class="bck"><div id="block_2" class="row rowNo06">
+
+<div id='start'>
+
+
+         <div id="bck_1" class="bck"><div id="block_1" class="row rowNo25">
+
+
+                              <div class="large-6 columns profile">
+                                  <div class="profile-picture">
+                                      <img class="imgPlace thumbnail" src="http://placehold.it/550x350">
+                                  </div>
+                                  <p class="profile-description">
+                                      <strong>Your business:</strong>
+                                      Have the freedom to work and earn as an independent business owner with a solid support base. Network with other businesses owners through
+                                      the franchise increasing sales and exposure of your artwork. Benefit from the marketing and advertising support provided giving your business
+                                  the support and guidance when you need it.</p>
+                              </div>
+
+
+                              <div class="large-6 columns profile">
+                                  <div class="profile-picture">
+                                      <img class="imgPlace thumbnail" src="http://placehold.it/550x350">
+                                  </div>
+                                  <p class="profile-description">
+                                      <strong>An ever growing network:</strong>
+                                      Networking with other artists, photographers and galleries is the fundamental key to growing your business. Connecting to a large ever growing
+                                      selection of images not only aids the exposure of your business on a national level but provides the opportunity for substantially increased revenue.
+
+                                  </p>
+                              </div>
+
+  </div></div><div id="bck_2" class="bck"><div id="block_2" class="row column text-center rowNo37">  <hr>
+  </div></div><div id="bck_3" class="bck"><div id="block_3" class="row rowNo06">
+
+
+  <div class="medium-4 columns">
+  <h3>Photoshop</h3>
+  <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-4 columns">
+  <h3>Javascript</h3>
+  <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-4 columns">
+  <h3>Marketing</h3>
+  <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  </div></div><div id="bck_4" class="bck"><div id="block_4" class="row rowNo06">
+
+
+  <div class="medium-4 columns">
+  <h3>Photoshop</h3>
+  <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-4 columns">
+  <h3>Javascript</h3>
+  <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  <div class="medium-4 columns">
+  <h3>Marketing</h3>
+  <p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna.</p>
+  </div>
+  </div></div><div id="bck_5" class="bck"><div id="block_5" class="row column text-center rowNo37">  <hr>
+  </div></div><div id="bck_6" class="bck"><div id="block_6" class="row rowNo17">    <div class="large-6 columns">
+          <div class="row">
+              <div class="large-4 columns">
+                  <img class="imgPlace" src="http://placehold.it/400x200">
+              </div>
+              <div class="large-8 columns">
+                  <h4>Illnesses & conditions</h4>
+                  <p>Bacon ipsum dolor sit amet nulla </p>
+              </div>
+          </div>
+      </div>
+
+      <div class="large-6 columns">
+          <div class="row">
+              <div class="large-4 columns">
+                  <img class="imgPlace" src="http://placehold.it/400x200">
+              </div>
+              <div class="large-8 columns">
+                  <h4>Injuries</h4>
+                  <p>Bacon ipsum dolor sit amet nulla </p>
+              </div>
+          </div>
+      </div>
+  </div></div><div id="bck_7" class="bck"><div id="block_7" class="row rowNo17">    <div class="large-6 columns">
+          <div class="row">
+              <div class="large-4 columns">
+                  <img class="imgPlace" src="http://placehold.it/400x200">
+              </div>
+              <div class="large-8 columns">
+                  <h4>Illnesses & conditions</h4>
+                  <p>Bacon ipsum dolor sit amet nulla </p>
+              </div>
+          </div>
+      </div>
+
+      <div class="large-6 columns">
+          <div class="row">
+              <div class="large-4 columns">
+                  <img class="imgPlace" src="http://placehold.it/400x200">
+              </div>
+              <div class="large-8 columns">
+                  <h4>Injuries</h4>
+                  <p>Bacon ipsum dolor sit amet nulla </p>
+              </div>
+          </div>
+      </div>
+  </div></div><div id="bck_8" class="bck"><div id="block_8" class="row rowNo17">    <div class="large-6 columns">
+          <div class="row">
+              <div class="large-4 columns">
+                  <img class="imgPlace" src="http://placehold.it/400x200">
+              </div>
+              <div class="large-8 columns">
+                  <h4>Illnesses & conditions</h4>
+                  <p>Bacon ipsum dolor sit amet nulla </p>
+              </div>
+          </div>
+      </div>
+
+      <div class="large-6 columns">
+          <div class="row">
+              <div class="large-4 columns">
+                  <img class="imgPlace" src="http://placehold.it/400x200">
+              </div>
+              <div class="large-8 columns">
+                  <h4>Injuries</h4>
+                  <p>Bacon ipsum dolor sit amet nulla </p>
+              </div>
+          </div>
+      </div>
+  </div></div><div id="bck_9" class="bck"><div id="block_9" class="row rowNo19">        <div class="large-9 large-centered columns">
+              <p class="text-center">Lorem ipsum dolor sit amet, nulla cras penatibus sodales nunc vestibulum ut, maecenas suscipit, mi sem dolor leo wisi id, leo vitae eget posuere purus aliquam morbi. Vulputate fusce sapien non, tristique in non amet ac vitae tristique, aliquam vivamus a, ipsum malesuada sagittis nulla, id praesent morbi rhoncus morbi luctus. Ligula odio enim sed nisl diam urna.
+            </p>
+         </div>
+  </div></div><div id="bck_10" class="bck"><div id="block_10" class="row rowNo27">
+      <!-- Contact Details -->
+      <div class="large-7 large-centered columns address">
+
+        <h5>Contact us</h5>
+        <p>If you have any questions or would like to know more about our services, we'd love to hear from you. <br so="" go="" on,="" drop="" us="" a="" line.<="" p="">
+
+
+          </p><section class="section">
+
+            <div class="content">
+              <form action="mail.php" method="post" onsubmit="return formValidator()">
+                <div class="row collapse">
+                  <div class="large-2 columns">
+                    <label class="inline">Your Name</label>
+                  </div>
+                  <div class="large-10 columns">
+                    <input type="text" name="name" id="yourName" placeholder="Jane Smith">
+                  </div>
+                </div>
+                <div class="row collapse">
+                  <div class="large-2 columns">
+                    <label class="inline"> Your Email</label>
+                  </div>
+                  <div class="large-10 columns">
+                    <input type="text" name="email" id="yourEmail" placeholder="jane@smithco.com">
+                  </div>
+                </div>
+                <div class="row collapse">
+                  <div class="large-2 columns">
+                    <label class="inline">Subject</label>
+                  </div>
+                  <div class="large-10 columns">
+                    <input type="text" name="subject" id="yourSubject" placeholder="subject">
+                  </div>
+                </div>
+                <label>Message</label>
+                <textarea name="message" rows="4"></textarea>
+                <button type="submit" class="radius button">Submit</button>
+              </form>
+            </div>
+          </section>
+          </div></div></div><div id="bck_11" class="bck"><div id="block_11" class="row columns rowNo32">
+  <header id="header">
+  	<h2>Another headline that is not really that interesting</h2>
+  	<ul class="icons">
+         <li><a href="#"><i class="fa fa-twitter fa-lg" aria-hidden="true"></i></a></li>
+         <li><a href="#"><i class="fa fa-facebook fa-lg" aria-hidden="true"></i></a></li>
+         <li><a href="#"><i class="fa fa-pinterest-p fa-lg" aria-hidden="true"></i></a></li>
+      </ul>
+  </header>
+  </div></div></div><?php include('includes/footer.inc.php'); ?>
